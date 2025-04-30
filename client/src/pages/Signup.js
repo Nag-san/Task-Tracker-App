73f14be88af547156f0960ae90b91673d07eab65
naveen_math <nagrajmath2@gmail.com>
2025-05-01 02:30:37 +0530

    Fixed page routes

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import API from "../../services/api.js";
+import API from "../services/api.js";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext.js";
 
