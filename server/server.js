26b1ba782a2f4c29eb8b8fd09b8f2775d8ca48e1
naveen_math <nagrajmath2@gmail.com>
2025-05-01 01:51:07 +0530

    Minor changes

--- server/server.js
+++ server/server.js
@@ -22,5 +22,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+console.log("Listening on PORT:", process.env.PORT);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
