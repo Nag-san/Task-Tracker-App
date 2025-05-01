141af8be00cfebff26fb2f1c89f72e1b23290484
naveen_math <nagrajmath2@gmail.com>
2025-05-01 11:23:16 +0530

    Added basic styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
+import './styles/global.css';
 import { AuthProvider } from "./context/AuthContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
